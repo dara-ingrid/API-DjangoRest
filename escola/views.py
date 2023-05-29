e32272850ec21c6d943058a923faa96763c8c9f2
Dara Ferreira <dara-ingrid@hotmail.com>
2023-05-29 20:25:03 -0300

    Cria viewsets

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,13 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import  Curso, Aluno
+from serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method=='GET':
-        aluno = {'id': 1, 'nome': 'Dara'}
-        return JsonResponse(aluno)
+class AlunosViewset(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewset(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
