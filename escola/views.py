c7ddbde181a98ac43193fc0519682bce4cd09765
Dara Ferreira <dara-ingrid@hotmail.com>
2023-05-29 20:46:14 -0300

    Configura urls

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from escola.models import  Curso, Aluno
-from serializer import AlunoSerializer, CursoSerializer
+from escola.serializer import AlunoSerializer, CursoSerializer
 
 class AlunosViewset(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
@@ -11,3 +11,4 @@ class CursosViewset(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    
--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+from django.urls import path, include
+from escola.views import AlunosViewset, CursosViewset
+from rest_framework import  routers
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewset, basename = 'Alunos')
+router.register('cursos', CursosViewset, basename = 'Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos),
+    path('', include(router.urls)),
 ]
