d35bde8828b0f7f97069e810d4e920e4053f9f77
Dara Ferreira <dara-ingrid@hotmail.com>
2023-05-29 21:12:46 -0300

    Ajusta campo, testa as rotas local no Postman

--- escola/serializer.py
+++ escola/serializer.py
@@ -5,7 +5,7 @@ from escola.models import Aluno, Curso
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
-        fields = ['id', 'nome', 'cpf', 'data_nascimento']
+        fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
 
 
 class CursoSerializer(serializers.ModelSerializer):
